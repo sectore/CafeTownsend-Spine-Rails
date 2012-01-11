82c84e919c6941a8e3f3409dc6d41798580dc02a
sectore <info@websector.de>
2012-01-11 14:36:33 +0100

    first commit - just a basic rails app without any content

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CafeTownsendSpineRails::Application.config.secret_token = '9b51e14b05bffeb3e1661ea1bc3788ecd97ed1c5d9a25733db97e788a78bde8e80a38c9c396ddb1bb580c834cde471cf73a9e18fba77bdf5339d84d54a160073'
